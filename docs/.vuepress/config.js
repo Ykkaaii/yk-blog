46ca5ec9d21101c12a4f184139744de5351195eb
Yaokkaaii <1311025947@qq.com>
2022-03-31 11:17:48 +0800

    feat: 初始化项目并完成简单的配置

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,49 @@
+module.exports = {
+  // 使用主题
+  theme: 'reco',
+  // 说明
+  description: '使用vue-press构建的前端博客',
+  // 网站标题
+  title: '阿楷的作业本',
+  // 语言设置
+  locales: {
+    '/': {
+      lang: 'zh-CN'
+    }
+  },
+  // 移动端优化
+  // 在移动端，搜索框在获得焦点时会放大，并且在失去焦点后可以左右滚动，这可以通过设置元来优化。
+  head: [
+    ['meta', { name: 'viewport', content: 'width=device-width,initial-scale=1,user-scalable=no' }]
+  ],
+  // 主题配置
+  themeConfig: {
+    // 开启第二个侧边栏
+    subSidebar: 'auto',
+    // 导航栏配置 TODO：思考下怎么通过脚本实现配置
+    nav: [
+      {
+        text: '学习笔记',
+          items: [
+          { text: '数据结构&算法', link: '/notes/structure/structure' },
+        ]
+      },
+    ],
+    // 侧边栏配置
+    sidebar: {
+      '/notes/structure/': [
+        {
+          title: "数据结构&算法",
+          path: '/notes/structure/structure',
+          collapsable: false, // 不折叠
+          children: [
+            { title: "数据结构", path: "/notes/structure/structure" },
+            { title: "算法", path: "/notes/structure/algorithm" }
+          ],
+        }
+      ],
+    },
+    // 最后更新时间
+    lastUpdated: '更新于: ',
+  },
+};
---
+++ docs/README.md
@@ -0,0 +1,58 @@
+---
+home: true
+heroImage: /logo.jpeg
+heroText: 阿楷的作业本
+tagline: 学不动了，阿Sir!!
+actionText: 从入门到入土 →
+actionLink: /zh/guide/
+title: 首页
+---
+
+# 约定文本规范
+
+## Front Matter
+
+front matter 必须是 markdown 文件中的第一部分，并且必须采用在三点划线之间书写的有效的 YAML。 这是约定在每篇文章必须的：
+```yaml
+---
+title: 文章标题
+author: 作者
+date: yyyy-MM-dd
+categories:
+ - 分类1
+ - 分类2
+tags:
+ - 标签1
+ - 标签2
+---
+```
+
+## Markdown书写约定
+为了文章的工整和可阅读性, 约定在每篇文章必须依照的书写格式：
+```md
+## 一级标题
+用于展示区分文章章节
+
+### 二级标题
+用于展示区分章节内的小节
+
+::: tip
+用于展示提示内容
+:::
+
+::: danger
+用于展示警告内容
+:::
+
+**加粗**
+用于标记重点文字
+
+`代码内容`
+用于标记正文中出现的代码内容
+
+(```代码类型(js/md/html...)
+用于展示代码块, 约定必须写上代码类型
+```)
+```
+
+
